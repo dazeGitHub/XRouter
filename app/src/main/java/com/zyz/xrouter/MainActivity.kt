68a65fb707b2690884b9d35b59075402de10d46b
mayundaze <1992330573@qq.com>
2022-03-06 20:33:14 +0800

    再次添加 annotation 和 annotation_compiler

--- app/src/main/java/com/zyz/xrouter/MainActivity.kt
+++ app/src/main/java/com/zyz/xrouter/MainActivity.kt
@@ -2,7 +2,9 @@ package com.zyz.xrouter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.toys.annotation.BindPath
 
+@BindPath("main/main")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
