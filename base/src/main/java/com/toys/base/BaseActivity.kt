d972b41fc26e140d2b2a0de296e90af0bb032b8f
zhaoyaze <zyz163mail@163.com>
2023-05-25 16:12:11 +0800

    修改版本号 5.0.9

--- base/src/main/java/com/toys/base/BaseActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.toys.base
-
-import androidx.appcompat.app.AppCompatActivity
-
-abstract class BaseActivity : AppCompatActivity() {
-    val TAG: String = this::class.java.name
-}
--- common/src/main/java/com/toys/common/bean/User.kt
+++
@@ -1,3 +0,0 @@
-package com.toys.common.bean
-
-data class User(val username:String, val age: Int)
